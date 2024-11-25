22b3dd3d1267da49e44e91e5417b5edd3ff0274b
Fabian Rostello <pv15osx@eduvaud.ch>
2024-11-25 11:41:18 +0100

    refactor: externalize the logic to ask and retrieve a float

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-from calendar import error
-
 operand1 = None
 operator = None
 operand2 = None
@@ -16,7 +14,7 @@ def ask_user_input():
 
     global operator
     # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /, ^): ")
+    operator = input("Enter an operator (+, -, *, /): ")
 
     global operand2
     # Get second operand from the user
@@ -36,8 +34,6 @@ def calculate(ope1, oper, ope2):
                 print("Error: Division by zero is undefined.")
                 return
             res = ope1 / ope2
-        case '^':
-            res = ope1 ** ope2
         case _:
             print("Invalid operator.")
             return
