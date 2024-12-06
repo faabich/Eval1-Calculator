4906e977c45ea718c3a46f3d6d78a1685420b799
Fabian Rostello <pv15osx@eduvaud.ch>
2024-12-06 11:33:40 +0100

    refractor: changed import logic

--- src/MathLib.py
+++ src/MathLib.py
@@ -5,20 +5,25 @@ class MathLib:
     def __init__ (self, res):
         self.res = res
 
-    match MathRequest.get_oper():
-        case '+':
-            res = MathRequest.get_ope1() + MathRequest.get_ope2()
-        case '-':
-            res = MathRequest.get_ope1() - MathRequest.get_ope2()
-        case '*':
-            res = MathRequest.get_ope1() * MathRequest.get_ope2()
-        case '/':
-            if MathRequest.get_ope2() == 0:
-                print("Error: Division by zero is undefined.")
-            else:
-                res = MathRequest.get_ope1() / MathRequest.get_ope2()
-        case '^':
-            res = MathRequest.get_ope1() ** MathRequest.get_ope2()
-        case _:
-            print("Invalid operator.")
-    MathRequest.set_res(res)
+    def calculate(MathRequest):
+        ope1 = MathRequest.get_ope1()
+        oper = MathRequest.get_oper()
+        ope2 = MathRequest.get_ope2()
+
+        match oper:
+            case '+':
+                res = ope1 + ope2
+            case '-':
+                res = ope1 - ope2
+            case '*':
+                res = ope1 * ope2
+            case '/':
+                if ope2 == 0:
+                    print("Error: Division by zero is undefined.")
+                else:
+                    res = ope1 / ope2
+            case '^':
+                res = ope1 ** ope2
+            case _:
+                print("Invalid operator.")
+        MathRequest.set_res(res)
